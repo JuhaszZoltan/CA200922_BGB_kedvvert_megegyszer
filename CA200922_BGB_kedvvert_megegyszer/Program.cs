d13d7adb31ae98f1130cce4f4fe6ee3c552dd70f
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-09-22 13:14:10 +0200

    add 100 star

--- CA200922_BGB_kedvvert_megegyszer/Program.cs
+++ CA200922_BGB_kedvvert_megegyszer/Program.cs
@@ -8,10 +8,22 @@ namespace CA200922_BGB_kedvvert_megegyszer
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
             Console.WriteLine("asd");
             Console.WriteLine("asd2");
+
+            for (int i = 0; i < 100; i++)
+            {
+                Console.SetCursorPosition(
+                    rnd.Next(Console.WindowWidth),
+                    rnd.Next(Console.WindowHeight));
+
+                Console.Write("*");
+            }
+
+            Console.ReadKey(true);
         }
     }
 }
