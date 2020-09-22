95564a92c35ced78a841e275e0a1a25b618a35b0
JuhaszZoltan <juhaszzoltanmate@yahoo.com>
2020-09-22 12:57:15 +0200

    Add project files.

---
+++ CA200922_BGB_kedvvert_megegyszer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA200922_BGB_kedvvert_megegyszer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("asd");
+        }
+    }
+}
